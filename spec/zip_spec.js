1638faa99cf29565c70c330367406c80fede3b78
Steve Conover <sconover@gmail.com>
2011-02-01 11:20:07 -0800

    zip with a function is like multi-each

--- spec/zip_spec.js
+++ spec/zip_spec.js
@@ -4,8 +4,20 @@ describe("zip", function() {
   
   beforeEach(function(){ this.cf = setupArrayEach() })
       
+  it("zip is like multi-each if you provide it a callback", function(){
+    var results = []
+    this.cf.zip([1,2,3], [4,5,6], [7,8,9], function(itemA, itemB, itemC, i) {
+      results.push([itemA, itemB, itemC, i])
+    })
+    expect(results).toEqual([
+      [1,4,7, 0],
+      [2,5,8, 1],
+      [3,6,9, 2]
+    ])
+  })
+      
   xit("zips up N collections, creating collections across matching indexes", function(){
-    var result = this.cf.equals([1,2,3], [4,5,6], [7,8,9])
+    var result = this.cf.zip([1,2,3], [4,5,6], [7,8,9])
     expect(result).toEqual([
       [1,4,7],
       [2,5,8],

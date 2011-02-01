d4e1b208aba8fe06c20d0185918e1c32c894b3d5
Steve Conover <sconover@gmail.com>
2011-01-31 23:41:48 -0800

    splice - overwrite

--- spec/splice_spec.js
+++ spec/splice_spec.js
@@ -9,5 +9,10 @@ describe("splice", function() {
     expect(result).toEqual([5,6,7,8,9,10,11])
   })
   
+  it("splice in another collection, overwriting some items in the original collection", function(){
+    var result = this.cf.splice([5,6,777,888,999,10,11], [7,8,9], 2, 3)
+    expect(result).toEqual([5,6,7,8,9,10,11])
+  })
+  
 })
 

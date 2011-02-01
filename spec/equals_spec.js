d41e57fa8a41aec42e80e6368a2c4a01269c7a09
Steve Conover <sconover@gmail.com>
2011-02-01 13:32:55 -0800

    equals implemented using across.detect

--- spec/equals_spec.js
+++ spec/equals_spec.js
@@ -4,7 +4,7 @@ describe("equals", function() {
   
   beforeEach(function(){ this.cf = setupArrayEach() })
       
-  xit("is equal if the contents are equal and in matching positions", function(){
+  it("is equal if the contents are equal and in matching positions", function(){
     expect(this.cf.equals([7,8,9,10], [7,8,9,10])).toEqual(true)
     
     expect(this.cf.equals([7,8,9,10,10], [7,8,9,10])).toEqual(false)
@@ -14,6 +14,11 @@ describe("equals", function() {
     expect(this.cf.equals([7,8,9,10], [7,8,9])).toEqual(false)
     
     expect(this.cf.equals([99,8,9,10], [7,8,9,10])).toEqual(false)
+  })
+      
+  it("costs what it takes to get to the first non-equal item", function(){
+    this.cf.equals([7,8,9,10], [7,8,9999,10])
+    expect(this.cf.lastCost()).toEqual(3)
   })
       
 })

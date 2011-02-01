8eecc5753cc58dce516021dd23be9ddeb6beb0d4
Steve Conover <sconover@gmail.com>
2011-02-01 15:56:41 -0800

    more comprehensive proof / example of decorate object style

--- spec/object_style_spec.js
+++ spec/object_style_spec.js
@@ -4,7 +4,7 @@ describe("functions are available, method-style, with the target object as the c
     
   it("works with a plain object", function(){
     var thing = {array:[]}
-    arrayEachCf().decorateObjectStyle(thing, function(){return this.array})
+    CollectionFunctions.Array.decorateObjectStyle(thing, function(){return this.array})
     
     thing.array.push(1)
     thing.array.push(2)
@@ -14,13 +14,38 @@ describe("functions are available, method-style, with the target object as the c
   
   it("works with a prototype", function(){
     var Thing = function() { this.array = [] }
-    arrayEachCf().decorateObjectStyle(Thing.prototype, function(){return this.array})
+    CollectionFunctions.Array.decorateObjectStyle(Thing.prototype, function(){return this.array})
     var thing = new Thing()
     
     thing.array.push(1)
     thing.array.push(2)
     var result = thing.map(function(item){return "x" + item})
     expect(result).toEqual(["x1", "x2"])
+  })
+
+  it("works with a prototype...returns new collections that are like the decorated object", function(){
+    var Thing = function() { this.array = [] }
+    
+    CollectionFunctions(
+      function(thing) {return CollectionFunctions.Array.functions.newIterator(thing.array)}, 
+      function(){return null}, 
+      function(a,b){return a==b},
+      function(){return new Thing()},
+      function(thing, item){thing.array.push(item)},
+      function(object){return object.constructor == Thing}
+    ).decorateObjectStyle(Thing.prototype, function(){return this})
+    
+    var thing = new Thing()
+    
+    thing.array.push(1)
+    thing.array.push(2)
+    var result = thing.map(function(item){return "x" + item})
+    expect(result.array).toEqual(["x1", "x2"])
+    
+    var otherThing = new Thing()
+    otherThing.array.push(3)
+    result = thing.concat(otherThing)
+    expect(result.array).toEqual([1,2,3])
   })
 
   it("expose array cf for convenience", function(){
--- spec/setup_array_each.js
+++ spec/setup_array_each.js
@@ -1,41 +1,5 @@
-arrayEachCf = function() {  
-  var arrayIterator = function(collection){
-    var position = 0
-    return {
-      next: function() {
-        var result = collection[position]
-        position += 1
-        return result
-      },
-      hasNext: function(){return position<collection.length}
-    }
-  }
-
-  var nothing = function(){
-    return null
-  }
-  
-  var doubleEquals = function(a,b){
-    return a == b
-  }
-  
-  var newArray = function(){
-    return []
-  }
-  
-  var arrayPush = function(array, item){
-    array.push(item)
-  }
-  
-  var isArray = function(thing){
-    return typeof thing.length != "undefined"
-  }
-  
-  return CollectionFunctions(arrayIterator, nothing, doubleEquals, newArray, arrayPush, isArray)
-}
-
 setupArrayEach = function() {  
   var cf = {}
-  arrayEachCf().decorate(cf)
+  CollectionFunctions.Array.decorate(cf)
   return cf
 }

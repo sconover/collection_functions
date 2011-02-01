88862e703db724b10f1ab3ca29bd80954a71c2f7
Steve Conover <sconover@gmail.com>
2011-01-31 23:21:05 -0800

    slice - start plus length

---
+++ spec/slice_spec.js
@@ -0,0 +1,26 @@
+require("./spec_helper.js");
+
+describe("slice", function() {
+  
+  beforeEach(function(){ this.cf = setupArrayEach() })
+      
+  it("slices part of a collection - start + length", function(){
+    var result = this.cf.slice([5,6,7,8], 1, 3)
+    expect(result).toEqual([6,7,8])
+
+    result = this.cf.slice([5,6,7,8], 1, 2)
+    expect(result).toEqual([6,7])
+  })
+  
+  xit("slices part of a collection - start + end range", function(){
+    result = this.cf.slice([5,6,7,8], [0, 2])
+    expect(result).toEqual([5,6,7])
+  })
+  
+  xit("slices part of a collection - range, where negative means work backwards from the end", function(){
+    result = this.cf.slice([5,6,7,8,9], [0, -2])
+    expect(result).toEqual([5,6,7,8])
+  })
+  
+})
+
---
+++ spec/splice_spec.js
@@ -0,0 +1,13 @@
+require("./spec_helper.js");
+
+describe("splice", function() {
+  
+  beforeEach(function(){ this.cf = setupArrayEach() })
+      
+  xit("splice in another collection at the given position", function(){
+    var result = this.cf.splice([5,6,10,11], [7,8,9], 2)
+    expect(result).toEqual([5,6,7,8,9,10,11])
+  })
+  
+})
+

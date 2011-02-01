63be8cfc44acd3b27543958c900b92ea63e25027
Steve Conover <sconover@gmail.com>
2011-01-31 23:40:05 -0800

    basic splice

--- spec/concat_spec.js
+++ spec/concat_spec.js
@@ -9,6 +9,11 @@ describe("concat", function() {
     expect(result).toEqual([4,5,6,7,8,9])
   })
   
+  it("make a new collection from N collections", function(){
+    var result = this.cf.concat([4,5,6], [7,8,9], [10,11])
+    expect(result).toEqual([4,5,6,7,8,9,10,11])
+  })
+  
   it("costs the number items in both collections", function(){
     this.cf.concat([4,5,6], [7,8,9])
     expect(this.cf.lastCost()).toEqual(6)
--- spec/splice_spec.js
+++ spec/splice_spec.js
@@ -4,7 +4,7 @@ describe("splice", function() {
   
   beforeEach(function(){ this.cf = setupArrayEach() })
       
-  xit("splice in another collection at the given position", function(){
+  it("splice in another collection at the given position", function(){
     var result = this.cf.splice([5,6,10,11], [7,8,9], 2)
     expect(result).toEqual([5,6,7,8,9,10,11])
   })

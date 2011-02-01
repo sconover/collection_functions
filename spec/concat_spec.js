523dfb4798325c3a850c3b1b21321ba0d2075a1d
Steve Conover <sconover@gmail.com>
2011-01-31 22:39:31 -0800

    concat

---
+++ spec/concat_spec.js
@@ -0,0 +1,18 @@
+require("./spec_helper.js");
+
+describe("concat", function() {
+  
+  beforeEach(function(){ this.cf = setupArrayEach() })
+      
+  it("make a new collection from two collections", function(){
+    var result = this.cf.concat([4,5,6], [7,8,9])
+    expect(result).toEqual([4,5,6,7,8,9])
+  })
+  
+  it("costs the number items in both collections", function(){
+    this.cf.concat([4,5,6], [7,8,9])
+    expect(this.cf.lastCost()).toEqual(6)
+  })
+    
+})
+

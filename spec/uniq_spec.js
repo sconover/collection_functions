31fe45ea7285b997e7aed055c5e9c353f41e1083
Steve Conover <sconover@gmail.com>
2011-02-02 15:50:48 -0800

    uniq feature requirements

--- spec/uniq_spec.js
+++ spec/uniq_spec.js
@@ -8,6 +8,35 @@ describe("uniq", function() {
     var result = fArr.uniq([7,8,9,10,10,11,10,9,10])
     expect(result).toEqual([7,8,9,10,11])
   })
+
+  describe("feature requirements", function(){
+
+    it("requires newCollection, iterator, append, nothing, and equals", function(){
+      expect(CollectionFunctions({}).functions.
+        uniq).toBeUndefined()
+        
+      expect(CollectionFunctions({iterator:fArr.iterator}).functions.
+        uniq).toBeUndefined()
+
+      expect(CollectionFunctions({newCollection:function(){return []}, 
+                                  iterator:fArr.iterator,
+                                  append:function(array, item){array.push(item)},
+                                  nothing:function(){return null},
+                                  equals:function(a,b){return a==b}}).functions.
+        uniq).toBeDefined()
+    })    
+
+    it("works with newCollection, iterator, nothing, and equals", function(){
+      var fMin = CollectionFunctions({newCollection:function(){return []}, 
+                                      iterator:fArr.iterator,
+                                      append:function(array, item){array.push(item)},
+                                      nothing:function(){return null},
+                                      equals:function(a,b){return a==b}}).functions
+      var result = fMin.uniq([7,8,9,10,10,11,10,9,10])
+      expect(result).toEqual([7,8,9,10,11])
+    })    
+
+  })
     
   it("cost is high.  this is inefficient for now.", function(){
     fArr.uniq([7,8,9,10,10,11])

1d15c7acf5750d6d098a12e6106db8e036a623b1
Steve Conover <sconover@gmail.com>
2011-02-02 15:09:52 -0800

    feature requirements for flatten

--- spec/flatten_spec.js
+++ spec/flatten_spec.js
@@ -13,6 +13,40 @@ describe("flatten", function() {
     var result = fArr.flatten([1,2,3,4,5,6,7])
     expect(result).toEqual([1,2,3,4,5,6,7])
   })
-    
+  
+  describe("feature requirements", function(){
+
+    it("requires iterator, newCollection, and isCollection", function(){
+      expect(CollectionFunctions({iterator:fArr.iterator}).functions.
+        flatten).toBeUndefined()
+
+      expect(CollectionFunctions({newCollection:function(){return []}}).functions.
+        flatten).toBeUndefined()
+
+      expect(CollectionFunctions({isCollection:function(thing){ return typeof thing.length != "undefined" }}).functions.
+        flatten).toBeUndefined()
+
+      expect(CollectionFunctions({append:function(array, item){array.push(item)}}).functions.
+        flatten).toBeUndefined()
+
+      expect(CollectionFunctions({iterator:fArr.iterator,
+                                  newCollection:function(){return []},
+                                  isCollection:function(thing){ return typeof thing.length != "undefined" },
+                                  append:function(array, item){array.push(item)}}).functions.
+        flatten).toBeDefined()
+    })    
+
+    it("works with iterator, newCollection, and isCollection", function(){
+      var fMin = CollectionFunctions({iterator:fArr.iterator,
+                                      newCollection:function(){return []},
+                                      isCollection:function(thing){ return typeof thing.length != "undefined" },
+                                      append:function(array, item){array.push(item)}}).functions
+                                      
+      var result = fMin.flatten([1,[2,[3,4]],5,[6,[7]]])
+      expect(result).toEqual([1,2,3,4,5,6,7])
+    })    
+
+  })
+  
 })
 

0f9bbc64218b501b7a0efea83d2065f546015278
Steve Conover <sconover@gmail.com>
2011-02-02 14:24:40 -0800

    all is like clone

--- spec/all_spec.js
+++ spec/all_spec.js
@@ -13,5 +13,67 @@ describe("all", function() {
     expect(results).toEqual([7,8,9])
   })
   
+  describe("feature requirements", function(){
+
+    it("requires newCollection and append features if you don't have a concat feature", function(){
+      expect(CollectionFunctions({}).functions.
+        all).toBeUndefined()
+        
+      expect(CollectionFunctions({newCollection:function(){return []}}).functions.
+        all).toBeUndefined()
+
+      expect(CollectionFunctions({newCollection:function(){return []}, 
+                                  append:function(array, item){array.push(item)}}).functions.
+        all).toBeDefined()
+    })    
+
+    it("is available if you supply a concat features", function(){
+      expect(CollectionFunctions({}).functions.
+        all).toBeUndefined()
+
+      expect(
+        CollectionFunctions({concat:function(){
+                              var firstArray = arguments[0]
+                              var otherArrays = []
+                              for(var i=1; i<arguments.length; i++) {otherArrays[i-1] = arguments[i]}
+                              return firstArray.concat.apply(firstArray, otherArrays)
+                            }}).functions.
+        all).toBeDefined()
+    })    
+
+  })
+  
+  
+  describe("cost", function(){
+    it("is N if no concat feature is specified", function(){
+      var fMin = minimalArrayCF().appendFeatures({
+        newCollection:function(){return []},
+        append:function(array, item){array.push(item)}
+      }).functions
+      expect(fMin.all([7,8,9])).toEqual([7,8,9])
+      expect(fMin.lastCost()).toEqual(3)
+    })
+
+    it("is zero if concat feature is specified", function(){
+      var fMin = minimalArrayCF().appendFeatures({
+        concat:function(){
+          var firstArray = arguments[0]
+          var otherArrays = []
+          for(var i=1; i<arguments.length; i++) {otherArrays[i-1] = arguments[i]}
+          return firstArray.concat.apply(firstArray, otherArrays)
+        }
+      }).functions
+      
+      expect(fMin.all([7,8,9])).toEqual([7,8,9])
+      expect(fMin.lastCost()).toEqual(0)
+    })
+
+    it("array cf has the concat feature specified and therefore is zero-cost", function(){
+      expect(fArr.all([7,8,9])).toEqual([7,8,9])
+      expect(fArr.lastCost()).toEqual(0)
+    })
+  })
+  
+  
 })
 

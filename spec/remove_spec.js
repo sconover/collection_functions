f8ef51d615c184045f6b8e41fc6257afadcbfcc1
Steve Conover <sconover@gmail.com>
2011-02-02 07:50:29 -0800

    remove.  minor rearranging.

---
+++ spec/remove_spec.js
@@ -0,0 +1,18 @@
+require("./spec_helper.js");
+
+describe("remove", function() {
+  
+  beforeEach(function(){ this.cf = setupArrayEach() })
+      
+  it("removes the value at an index", function(){
+    var result = this.cf.remove([7,8,9,10], 2)
+    expect(result).toEqual([7,8,10])
+  })
+    
+  it("cost is the length of the collection due to creation of new collection", function(){
+    this.cf.remove([7,8,9,10], 2)
+    expect(this.cf.lastCost()).toEqual(4)
+  })
+    
+})
+

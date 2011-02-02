88a76257308fa4ad468d07f5328066bb6aba062a
Steve Conover <sconover@gmail.com>
2011-02-02 15:01:15 -0800

    equals requirements

--- spec/equals_spec.js
+++ spec/equals_spec.js
@@ -15,11 +15,54 @@ describe("equals", function() {
     
     expect(fArr.equals([99,8,9,10], [7,8,9,10])).toEqual(false)
   })
-      
-  it("costs what it takes to get to the first non-equal item", function(){
-    fArr.equals([7,8,9,10], [7,8,9999,10])
-    expect(fArr.lastCost()).toEqual(3)
+  
+  describe("feature requirements", function(){
+
+    it("requires iterator, nothing, equals, append, newCollection", function(){
+      expect(CollectionFunctions({nothing:function(){return null}}).functions.
+        equals).toBeUndefined()
+
+      expect(CollectionFunctions({iterator:fArr.iterator}).functions.
+        equals).toBeUndefined()
+
+      expect(CollectionFunctions({equals:function(a,b){return a==b}}).functions.
+        equals).toBeUndefined()
+
+      expect(CollectionFunctions({newCollection:function(){return []}}).functions.
+        equals).toBeUndefined()
+
+      expect(CollectionFunctions({append:function(array, item){array.push(item)}}).functions.
+        equals).toBeUndefined()
+
+      expect(CollectionFunctions({nothing:function(){return null}, 
+                                  iterator:fArr.iterator,
+                                  equals:function(a,b){return a==b},
+                                  append:function(array, item){array.push(item)},
+                                  newCollection:function(){return []}}).functions.
+        equals).toBeDefined()
+    })    
+
+    it("works with iterator, nothing, equals, append, newCollection", function(){
+      var fMin = CollectionFunctions({iterator:fArr.iterator, 
+                                      nothing:function(){return null}, 
+                                      equals:function(a,b){return a==b},
+                                      append:function(array, item){array.push(item)},
+                                      newCollection:function(){return []}}).functions
+      expect(fMin.equals([8,9,10], [8,9,10])).toEqual(true)
+      expect(fMin.equals([8,9,10], [0,1,2,3])).toEqual(false)
+    })    
+
   })
+  
+  
+  describe("cost", function(){
+    
+    it("costs what it takes to get to the first non-equal item", function(){
+      fArr.equals([7,8,9,10], [7,8,9999,10])
+      expect(fArr.lastCost()).toEqual(3)
+    })
+    
+  })    
       
 })
 

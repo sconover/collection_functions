6dead508b4d5f6a110848e7311e69ce17de48b20
Steve Conover <sconover@gmail.com>
2011-02-01 22:40:09 -0800

    without

---
+++ spec/without_spec.js
@@ -0,0 +1,13 @@
+require("./spec_helper.js");
+
+describe("without", function() {
+  
+  beforeEach(function(){ this.cf = setupArrayEach() })
+      
+  it("returns a new collection without the item", function(){
+    var result = this.cf.without([7,7,8,9,10,10,11], 10)
+    expect(result).toEqual([7,7,8,9,11])
+  })
+    
+})
+

e5182a5787206c612fc8c33f8ba6f2a2c62bc8a6
Steve Conover <sconover@gmail.com>
2011-02-02 20:59:54 -0800

    moving collection tests into their own place.

--- spec/collection/spec_helper.js
+++ spec/collection/spec_helper.js
@@ -1,7 +1,7 @@
 require.paths.push("spec")
 require.paths.push("lib")
 
-require("../../jasmine-node/lib/jasmine")
+require("../../../jasmine-node/lib/jasmine")
 
 for(var key in jasmine) {
   global[key] = jasmine[key]

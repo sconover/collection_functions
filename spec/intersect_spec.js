53fd8864d1a8a40f15b67a70a100f13c3f5f666a
Steve Conover <sconover@gmail.com>
2011-02-02 16:11:52 -0800

    intersect, differ feature requirements, rf

--- spec/intersect_spec.js
+++ spec/intersect_spec.js
@@ -8,6 +8,35 @@ describe("intersect", function() {
     var result = fArr.intersect([7,8,9,10,10,11], [8,10,10])
     expect(result).toEqual([8,10])
   })
+
+  describe("feature requirements", function(){
+
+    it("requires newCollection, iterator, append, nothing, and equals", function(){
+      expect(CollectionFunctions({}).functions.
+        intersect).toBeUndefined()
+        
+      expect(CollectionFunctions({iterator:fArr.iterator}).functions.
+        intersect).toBeUndefined()
+
+      expect(CollectionFunctions({newCollection:function(){return []}, 
+                                  iterator:fArr.iterator,
+                                  append:function(array, item){array.push(item)},
+                                  nothing:function(){return null},
+                                  equals:function(a,b){return a==b}}).functions.
+        intersect).toBeDefined()
+    })    
+
+    it("works with newCollection, iterator, nothing, and equals", function(){
+      var fMin = CollectionFunctions({newCollection:function(){return []}, 
+                                      iterator:fArr.iterator,
+                                      append:function(array, item){array.push(item)},
+                                      nothing:function(){return null},
+                                      equals:function(a,b){return a==b}}).functions
+      var result = fMin.intersect([7,8,9,10,10,11], [8,10,10])
+      expect(result).toEqual([8,10])
+    })    
+
+  })
     
   it("cost is high.  this is inefficient for now.", function(){
     fArr.intersect([7,8,9,10,10,11], [8,10,10])

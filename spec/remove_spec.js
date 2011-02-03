76f9d71180da6976af9cf2e19a8e6f707a095af6
Steve Conover <sconover@gmail.com>
2011-02-02 16:22:01 -0800

    remove feature requirements

--- spec/remove_spec.js
+++ spec/remove_spec.js
@@ -13,6 +13,33 @@ describe("remove", function() {
     fArr.remove([7,8,9,10], 2)
     expect(fArr.lastCost()).toEqual(4)
   })
+
+  describe("feature requirements", function(){
+
+    it("requires newCollection, append, and iterator", function(){
+      expect(CollectionFunctions({}).functions.
+        remove).toBeUndefined()
+      
+      expect(CollectionFunctions({iterator:fArr.iterator}).functions.
+        remove).toBeUndefined()
+      
+      expect(CollectionFunctions({newCollection:function(){return []},
+                                  iterator:fArr.iterator,
+                                  append:function(array, item){array.push(item)}}).functions.
+        remove).toBeDefined()
+    })    
+
+    it("works with newCollection, append, and iterator", function(){
+      var fMin = CollectionFunctions({iterator:fArr.iterator,
+                                      newCollection:function(){return []},
+                                      append:function(array, item){array.push(item)}}).functions
+
+      var result = fMin.remove([7,8,9,10], 2)
+      expect(result).toEqual([7,8,10])
+    })    
+
+  })
+
     
 })
 

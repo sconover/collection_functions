be2cb1c617439858877bd4decfc659543f60a1e7
Steve Conover <sconover@gmail.com>
2011-02-02 16:35:17 -0800

    without feature requirements.  done.

--- spec/without_spec.js
+++ spec/without_spec.js
@@ -8,6 +8,33 @@ describe("without", function() {
     var result = fArr.without([7,7,8,9,10,10,11], 10)
     expect(result).toEqual([7,7,8,9,11])
   })
+
+  describe("feature requirements", function(){
+
+    it("requires newCollection, iterator, equals, and append", function(){
+      expect(CollectionFunctions({}).functions.
+        select).toBeUndefined()
+        
+      expect(CollectionFunctions({iterator:fArr.iterator}).functions.
+        select).toBeUndefined()
+
+      expect(CollectionFunctions({newCollection:function(){return []}, 
+                                  iterator:fArr.iterator,
+                                  append:function(array, item){array.push(item)},
+                                  equals:function(a,b){return a==b}}).functions.
+        select).toBeDefined()
+    })    
+
+    it("works with newCollection, iterator, and append", function(){
+      var fMin = CollectionFunctions({newCollection:function(){return []}, 
+                                      iterator:fArr.iterator,
+                                      append:function(array, item){array.push(item)},
+                                      equals:function(a,b){return a==b}}).functions
+      var result = fMin.without([7,7,8,9,10,10,11], 10)
+      expect(result).toEqual([7,7,8,9,11])
+    })    
+
+  })
     
 })
 

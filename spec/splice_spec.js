724cf69d09184df1776020bc34078b3178389d3e
Steve Conover <sconover@gmail.com>
2011-02-02 16:32:13 -0800

    splice feature requirements

--- spec/splice_spec.js
+++ spec/splice_spec.js
@@ -13,6 +13,33 @@ describe("splice", function() {
     var result = fArr.splice([5,6,777,888,999,10,11], [7,8,9], 2, 3)
     expect(result).toEqual([5,6,7,8,9,10,11])
   })
+
+  describe("feature requirements", function(){
+
+    it("requires newCollection, append, and iterator", function(){
+      expect(CollectionFunctions({}).functions.
+        splice).toBeUndefined()
+      
+      expect(CollectionFunctions({iterator:fArr.iterator}).functions.
+        splice).toBeUndefined()
+      
+      expect(CollectionFunctions({newCollection:function(){return []},
+                                  iterator:fArr.iterator,
+                                  append:function(array, item){array.push(item)}}).functions.
+        splice).toBeDefined()
+    })    
+
+    it("works with newCollection, append, and iterator", function(){
+      var fMin = CollectionFunctions({iterator:fArr.iterator,
+                                      newCollection:function(){return []},
+                                      append:function(array, item){array.push(item)}}).functions
+      
+      var result = fMin.splice([5,6,10,11], [7,8,9], 2)
+      expect(result).toEqual([5,6,7,8,9,10,11])
+    })    
+
+  })
+
   
 })
 

6bc19b24970cc97fed459ce1933f9871450dc21b
Steve Conover <sconover@gmail.com>
2011-02-02 16:03:07 -0800

    inspect feature requirements

--- spec/inspect_spec.js
+++ spec/inspect_spec.js
@@ -13,6 +13,25 @@ describe("inspect", function() {
   it("outputs the string form of the value if there's no inspect function", function(){
     expect(fArr.inspect([1,2])).toEqual("1,2")
   })
+
+  describe("feature requirements", function(){
+
+    it("requires iterator", function(){
+      expect(CollectionFunctions({}).functions.
+        inspect).toBeUndefined()
+        
+      expect(CollectionFunctions({iterator:fArr.iterator}).functions.
+        inspect).toBeDefined()
+    })    
+
+    it("works with iterator", function(){
+      var fMin = CollectionFunctions({iterator:fArr.iterator}).functions
+      var one = {inspect:function(){return "one"}}
+      var two = {inspect:function(){return "two"}}
+      expect(fMin.inspect([one, two])).toEqual("one,two")
+    })    
+
+  })
       
 })
 
